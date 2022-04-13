2784ccead405c847730da89caa7e97d7634b365c
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-04-13 16:18:09 +0530

    resumed

--- lib/ui/screens/platforms.dart
+++ lib/ui/screens/platforms.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dio/dio.dart';
+
 class PlatformsScreen extends StatefulWidget {
   const PlatformsScreen({Key? key}) : super(key: key);
 
@@ -8,10 +10,54 @@ class PlatformsScreen extends StatefulWidget {
 }
 
 class _PlatformsScreenState extends State<PlatformsScreen> {
+  List<String> platforms = [];
+  void getPlatforms() async {
+    final dio = Dio();
+    try {
+      var response = await dio.get("https://kontests.net/api/v1/sites");
+      var resArray = response.data.toList();
+      print(resArray);
+      // List<String> platforms = [];
+      for (var i = 0; i < resArray.length; i++) {
+        platforms.add(resArray[i][0].toString());
+      }
+      setState(() {});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPlatforms();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black.withOpacity(0.8),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Platforms"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Center(
+          child: (platforms.isEmpty)
+              ? const CircularProgressIndicator()
+              : ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.teal.withOpacity(0.8),
+                      width: 150,
+                      height: 75,
+                      child: Center(child: Text(platforms[index])),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemCount: platforms.length),
+        ),
+      ),
     );
   }
 }
