f59c6dc1c6aa24700d6a8b32f04ff26c4402e5b7
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-04-17 19:52:47 +0530

    confetti op

--- lib/ui/screens/platforms.dart
+++ lib/ui/screens/platforms.dart
@@ -5,6 +5,7 @@ import 'package:code_karo/ui/screens/utils.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dio/dio.dart';
+import 'package:confetti/confetti.dart';
 
 class PlatformsScreen extends StatefulWidget {
   const PlatformsScreen({Key? key}) : super(key: key);
@@ -15,6 +16,8 @@ class PlatformsScreen extends StatefulWidget {
 
 class _PlatformsScreenState extends State<PlatformsScreen> {
   List<Map<String, String>> platforms = [];
+
+  late ConfettiController _confettiController;
   void getPlatforms() async {
     final dio = Dio();
     try {
@@ -37,6 +40,10 @@ class _PlatformsScreenState extends State<PlatformsScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    _confettiController = ConfettiController(
+      duration: const Duration(seconds: 5),
+    );
 
     AwesomeNotifications().isNotificationAllowed().then(
       (isAllowed) {
@@ -82,6 +89,13 @@ class _PlatformsScreenState extends State<PlatformsScreen> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _confettiController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -89,62 +103,86 @@ class _PlatformsScreenState extends State<PlatformsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Center(
-          child: (platforms.isEmpty)
-              ? const CircularProgressIndicator(
-                  color: Colors.white,
-                )
-              : ListView.separated(
-                  itemBuilder: (context, index) {
-                    String platformName = platforms[index].keys.elementAt(0);
-                    String? platformCode = platforms[index][platformName];
-                    return GestureDetector(
-                      child: Container(
-                        color: Colors.teal.withOpacity(0.8),
-                        width: 150,
-                        height: 75,
-                        child: Center(
-                          child: Text(platformName),
-                        ),
-                      ),
-                      onTap:
-                          // LocalNotifs.createDummyNotif,
-                          //     () {
-                          //   Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //       builder: (context) =>
-                          //           EventsScreen(platformCode: platformCode!),
-                          //     ),
-                          //   );
-                          // },
+        child: Stack(
+          children: [
+            Center(
+              child: (platforms.isEmpty)
+                  ? const CircularProgressIndicator(
+                      color: Colors.white,
+                    )
+                  : ListView.separated(
+                      itemBuilder: (context, index) {
+                        String platformName =
+                            platforms[index].keys.elementAt(0);
+                        String? platformCode = platforms[index][platformName];
+                        return GestureDetector(
+                          child: Container(
+                            color: Colors.teal.withOpacity(0.8),
+                            width: 150,
+                            height: 75,
+                            child: Center(
+                              child: Text(platformName),
+                            ),
+                          ),
+                          onTap:
+                              // LocalNotifs.createDummyNotif,
+                              //     () {
+                              //   Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //       builder: (context) =>
+                              //           EventsScreen(platformCode: platformCode!),
+                              //     ),
+                              //   );
+                              // },
 
-                          () async {
-                        NotificationWeekAndTime? pickedSchedule =
-                            await pickSchedule(context);
+                              () async {
+                            NotificationWeekAndTime? pickedSchedule =
+                                await pickSchedule(context);
 
-                        if (pickedSchedule != null) {
-                          LocalNotifs.createWaterReminderNotification(
-                                  pickedSchedule)
-                              .then((value) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text(
-                                  "Reminder created! ✌️",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                duration: Duration(milliseconds: 1200),
-                                backgroundColor: Colors.black,
-                              ),
-                            );
-                          });
-                        }
+                            if (pickedSchedule != null) {
+                              LocalNotifs.createWaterReminderNotification(
+                                      pickedSchedule)
+                                  .then((value) {
+                                // ScaffoldMessenger.of(context).showSnackBar(
+                                //   const SnackBar(
+                                //     content: Text(
+                                //       "Reminder created! ✌️",
+                                //       style: TextStyle(color: Colors.white),
+                                //     ),
+                                //     duration: Duration(milliseconds: 1200),
+                                //     backgroundColor: Colors.black,
+                                //   ),
+                                // );
+                                _confettiController.play();
+                              });
+                            }
+                          },
+                        );
                       },
-                    );
-                  },
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemCount: platforms.length,
-                ),
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemCount: platforms.length,
+                    ),
+            ),
+            ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirectionality: BlastDirectionality
+                  .explosive, // don't specify a direction, blast randomly
+              // shouldLoop: true, // start again as soon as the animation is finished
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink,
+                Colors.orange,
+                Colors.purple
+              ],
+              maxBlastForce: 200,
+              minBlastForce: 100, // manually specify the colors to be used
+              gravity: 0.7,
+              numberOfParticles: 200,
+              // createParticlePath: drawStar, // define a custom shape/path.
+            )
+          ],
         ),
       ),
     );
